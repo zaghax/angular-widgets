b6be3c82fec4405a207434eeff7187ae4741efb1
Giovanny Ortega Arias <gortegaa@caracoltv.com.co>
2018-05-18 14:13:51 -0500

    Ajustes a la grafica de estadisticas

--- src/app/components/w-statistics/w-statistics.component.ts
+++ src/app/components/w-statistics/w-statistics.component.ts
@@ -13,12 +13,14 @@ export class WStatisticsComponent {
     {data: [10, 30, 60, 80, 56, 55, 40], label: 'Market Cap'}
   ];
 
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels:Array<any> = ['0', '1', '2', '3', '4', '5', '6'];
+  
   public lineChartOptions:any = {
     responsive: true
   };
+  
   public lineChartColors:Array<any> = [
-    { // grey
+    {
       backgroundColor: 'rgba(255,255,255,0.2)',
       borderColor: 'rgba(255,255,255,1)',
       pointBackgroundColor: 'rgba(255,255,255,1)',
@@ -27,7 +29,7 @@ export class WStatisticsComponent {
       pointHoverBorderColor: 'rgba(255,255,255,0.8)'
     }
   ];
-  // public lineChartLegend:boolean = true;
+  
   public lineChartType:string = 'line';
   
   constructor() { }
